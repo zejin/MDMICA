f02cd3066bdab82897bcaae6318e62c3b09063c9
Ze Jin <btluck1989@gmail.com>
2018-02-12 13:05:11 -0500

    test

--- R/mdm_ica.R
+++ R/mdm_ica.R
@@ -8,9 +8,9 @@
 #'   If omitted, an adaptive number is used.
 #' @param type The type of mutual dependence measures, including
 #' \itemize{
-#'   \item \code{asym} asymmetric measure \eqn{\mathcal{R}_n} based on distance covariance 
+#'   \item \code{asym}: asymmetric measure \eqn{\mathcal{R}_n} based on distance covariance 
 #'     \eqn{\mathcal{V}_n};
-#'   \item \code{sym}: symmectric measure \eqn{\mathcal{S}_n} based on distance covariance 
+#'   \item \code{sym}: symmetric measure \eqn{\mathcal{S}_n} based on distance covariance 
 #'     \eqn{\mathcal{V}_n};
 #'   \item \code{comp}: simplified complete measure \eqn{\mathcal{Q}_n^\star} based on 
 #'     incomplete V-statistics;
